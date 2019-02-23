dda4c00154d5548fc860784898121067ca68963f
neil aldrei <neilaldrei@icloud.com>
2019-02-23 14:57:30 +0800

    Added Routes

--- src/router.js
+++ src/router.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Profile from "./views/Profile";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
+      path: "/",
+      name: "profile",
+      component: Profile
     }
-  ]
+  ],
+  mode: "history"
 });
