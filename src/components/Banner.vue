f7a5a89f31c2259a5df8127ac9a5f91292714989
neil aldrei <neilaldrei@icloud.com>
2019-02-23 15:35:59 +0800

    Finished Banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="banner">
+        <div class="profile-info">
+            <div class="dp-container">
+                <div class="dp"></div>
+            </div>
+            <div class="main-info">
+                <h2>Peralta</h2>
+                <h1>Neil Aldrei</h1>
+                <h6>
+                    <i>Web Developer</i>
+                </h6>
+            </div>
+            <div class="profile-social">
+                <a href="#">G</a>
+                <a href="#">T</a>
+                <a href="#">M</a>
+            </div>
+        </div>
+        <div class="profile-contacts">
+            <h3>Naga City, Camarines Sur, PH</h3>
+            <h3>neilaldreiii@gmail.com</h3>
+            <h3>+63915-204-4704</h3>
+            <a href="#">Resume</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
