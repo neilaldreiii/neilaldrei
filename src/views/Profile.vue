115a50597f6ce97166f4d0ff441eb14d0dc29bc4
neil aldrei <neilaldrei@icloud.com>
2019-02-23 15:05:30 +0800

    Renamed most components, Added Contact component

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,34 @@
 <template>
-    
+    <div class="container">
+        <div class="col">
+            <app-intro></app-intro>
+            <app-timeline></app-timeline>
+        </div>
+        <div class="co">
+            <app-skills></app-skills>
+            <app-contacts></app-contacts>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+import Banner from "@/components/Banner";
+import Timeline from "@/components/Timeline";
+import Intro from "@/components/Intro";
+import Skills from "@/components/Skills";
+import Portfolio from "@/components/Portfolio";
+import Contacts from "@/components/Contacts";
 
+export default {
+    name: "profile",
+    components: {
+        "app-banner": Banner,
+        "app-timeline": Timeline,
+        "app-intro": Intro,
+        "app-skills": Skills,
+        "app-portfolio": Portfolio,
+        "app-contacts": Contacts
+    }
 }
 </script>
 
