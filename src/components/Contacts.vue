84f5e29fa88ada77ca82ce2d5bb6685f3aab418a
neil aldrei <neilaldrei@icloud.com>
2019-03-02 10:00:49 +0800

    Added Contacts

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -4,14 +4,27 @@
             <h1>Contacts</h1>
         </header>
         <div class="content">
-
+            <div class="contacts">
+                <p>Naga City, Camarines Sur, PH</p>
+                <p>neilaldreiii@gmail.com</p>
+                <p>+63915-204-4704</p>
+            </div>
+            <div class="social">
+                <a href="#">G</a>
+                <a href="#">T</a>
+                <a href="#">M</a>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    name: "contacts",
+    data() {
+        return {
+        }
+    }
 }
 </script>
 
