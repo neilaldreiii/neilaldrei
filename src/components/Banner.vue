f380cfc5d5cc0142a4f866e8f093dc25e259180f
neil aldrei <neilaldrei@icloud.com>
2019-03-03 08:57:06 +0800

    Tweaked some styles

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,6 +4,13 @@
             <div class="dp-container">
                 <div class="dp"></div>
             </div>
+            <div class="profile-social">
+                <a href="#">G</a>
+                <a href="#">T</a>
+                <a href="#">M</a>
+            </div>
+        </div>
+        <div class="profile-info">
             <div class="main-info">
                 <h2>Peralta</h2>
                 <h1>Neil Aldrei</h1>
@@ -11,17 +18,12 @@
                     <i>Web Developer</i>
                 </h6>
             </div>
-            <div class="profile-social">
-                <a href="#">G</a>
-                <a href="#">T</a>
-                <a href="#">M</a>
+            <div class="profile-contacts">
+                <h3>Naga City, Camarines Sur, PH</h3>
+                <h3>neilaldreiii@gmail.com</h3>
+                <h3>+63915-204-4704</h3>
+                <a href="#">Resume</a>
             </div>
-        </div>
-        <div class="profile-contacts">
-            <h3>Naga City, Camarines Sur, PH</h3>
-            <h3>neilaldreiii@gmail.com</h3>
-            <h3>+63915-204-4704</h3>
-            <a href="#">Resume</a>
         </div>
     </div>
 </template>
