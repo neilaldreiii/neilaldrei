d9d857512e3fc2f1d2cf3a33a2732aad692a3721
neil aldrei <neilaldrei@icloud.com>
2019-03-10 11:53:55 +0800

    Fixed introduction

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -5,7 +5,7 @@
         </header>
         <div class="content">
             <p>
-                <span style="margin-left:40px;"></span> Hello! My name is <b>Neil Aldrei</b>. I'm a BSIT student at University of Nueva Caceres. I do freelance web development work from time to time. If you need help creating or finishing your website, I'll be glad to help.
+                <span style="margin-left:40px;"></span> Hello! My name is <b>Neil Aldrei</b>. Self Taught Web Developer. I do freelance web development work from time to time. If you need help creating or finishing your website, I'll be glad to help.
             </p>
         </div>
     </div>
