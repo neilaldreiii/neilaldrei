d803eec86d19b865dce8415d1d6c478a08b14f34
neil aldrei <neilaldrei@icloud.com>
2019-03-17 17:41:12 +0800

    Added Map

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,19 +1,24 @@
 <template>
-    <div class="app-footer">
-        <footer>
-            <p class="copy">&copy; 2019 neilaldrei</p>
+    <footer>
+        <div class="footer-wrap">
             <div class="footer-contacts">
-                <p>Naga City, Camarines Sur, PH</p>
-                <p>neilaldreiii@gmail.com</p>
-                <p>+63915-204-4704</p>
+                <div class="footer-contacts-main">
+                    <p class="copy">&copy; 2019 neilaldrei</p>
+                    <p>Naga City, Camarines Sur, PH</p>
+                    <p>neilaldreiii@gmail.com</p>
+                    <p>+63915-204-4704</p>
+                </div>
+                <div class="footer-social">
+                    <a href="https://github.com/neilaldreiii" target="_blank" class="github icon"><i class="fab fa-github"></i></a>
+                    <a href="https://twitter.com/neilaldrei" target="_blank" class="twitter icon"><i class="fab fa-twitter"></i></a>
+                    <a href="https://www.messenger.com/t/dreiiin" target="_blank" class="mes icon"><i class="fab fa-facebook-messenger"></i></a>
+                </div>
             </div>
-            <div class="footer-social">
-                <a href="https://github.com/neilaldreiii" target="_blank" class="github icon"><i class="fab fa-github"></i></a>
-                <a href="https://twitter.com/neilaldrei" target="_blank" class="twitter icon"><i class="fab fa-twitter"></i></a>
-                <a href="https://www.messenger.com/t/dreiiin" target="_blank" class="mes icon"><i class="fab fa-facebook-messenger"></i></a>
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62036.47915405415!2d123.23544447774228!3d13.640727408333278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a18cad50331fcb%3A0x1dd856f8b79915ca!2sNaga%2C+Camarines+Sur!5e0!3m2!1sen!2sph!4v1552815121360" width="600" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
-        </footer>
-    </div>
+        </div>
+    </footer>
 </template>
 
 <script>
