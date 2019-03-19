10adea55983a7163533ad59bfc12ad59f60c42ab
neil aldrei <neilaldrei@icloud.com>
2019-03-19 20:19:14 +0800

    Removed Map

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -14,9 +14,6 @@
                     <a href="https://www.messenger.com/t/dreiiin" target="_blank" class="mes icon"><i class="fab fa-facebook-messenger"></i></a>
                 </div>
             </div>
-            <div class="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62036.47915405415!2d123.23544447774228!3d13.640727408333278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a18cad50331fcb%3A0x1dd856f8b79915ca!2sNaga%2C+Camarines+Sur!5e0!3m2!1sen!2sph!4v1552815121360" width="600" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div>
         </div>
     </footer>
 </template>
