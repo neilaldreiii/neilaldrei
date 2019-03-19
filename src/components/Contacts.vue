4d12283da07e1a82df2c69315d47d46888b7c5a2
neil aldrei <neilaldrei@icloud.com>
2019-03-19 20:03:50 +0800

    Finished Contacts

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -25,22 +25,27 @@
                     <div class="form">
                         <form v-on:submit.prevent="directMessage">
                             <div class="form-group">
-                                <input type="text" placeholder="First Name" v-model="firstName" class="form-control">
-                                <input type="text" placeholder="Last Name" v-model="lastName" class="form-control">
+                                <input type="text" placeholder="First Name" v-model="details.firstName" class="form-control">
+                                <input type="text" placeholder="Last Name" v-model="details.lastName" class="form-control">
                             </div>
                             <div class="form-group">
-                                <input type="text" placeholder="Email" v-model="email" class="form-control">
+                                <input type="text" placeholder="Email" v-model="details.email" class="form-control">
                             </div>
                             <div class="form-group">
-                                <textarea placeholder="Message" cols="30" rows="10" v-model="message" class="form-control"></textarea>
+                                <textarea placeholder="Message" cols="30" rows="10" v-model="details.message" class="form-control"></textarea>
                             </div>
                             <div class="form-action">
-                                <button type="submit">Send </button>
+                                <button type="submit" :disabled="sent" :class="{ sent : sent }">Send</button>
                             </div>
                         </form>
                     </div>
-                    <div class="success">
-                        <p>Thank you. Your message has been sent successfully.</p>
+                    <div v-if="alerts.length" class="alerts">
+                        <ul>
+                            <li v-for="alert in alerts" :key="alert.index"><p>{{ alert }}</p></li>
+                        </ul>
+                    </div>
+                    <div v-if="success.length" class="success">
+                        <p v-for="s in success" :key="s.index">{{ s }}</p>
                     </div>
                 </div>
             </div>
@@ -55,6 +60,73 @@ export default {
     name: "contacts",
     data() {
         return {
+            details: {
+                firstName: "",
+                lastName: "",
+                email: "",
+                message: ""
+            },
+            sent: false,
+            alerts: [],
+            success: []
+        }
+    },
+    methods: {
+        directMessage() {
+            
+            this.alerts = [];
+
+            if(!this.details.firstName) {
+
+                this.alerts.push("*First name is required");
+
+            } else if (!this.details.lastName) {
+
+                this.alerts.push("*Last name is required");
+
+            } else if (!this.details.email) {
+
+                this.alerts.push("*Email is required");
+
+            } else if (!this.details.message) {
+
+                this.alerts.push("*Message is required");
+
+            } else {
+
+                db.collection("userMessages")
+                .add({
+                    
+                    firstName: this.details.firstName,
+                    lastName: this.details.lastName,
+                    email: this.details.email,
+                    message: this.details.message
+                
+                })
+                .then(
+                    
+                    docRef => {
+
+                        this.sent = true;
+                        this.success.push("Message sent! Thank you for visiting my profile :) ");
+                        this.clearForm();
+
+                })
+                .catch(
+
+                    error => this.alerts.push(error.message)
+
+                )
+
+            }
+        },
+        clearForm() {
+
+            this.details.firstName = "";
+            this.details.lastName = "";
+            this.details.email = "";
+            this.details.message = "";
+
         }
     }
 }
